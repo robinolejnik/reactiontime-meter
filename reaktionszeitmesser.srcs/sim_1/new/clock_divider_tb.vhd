2023e722f332806d324625f654ee7f8ebe4d3ab3
Robin Olejnik <robinolejnik@users.noreply.github.com>
2022-12-26 20:06:13 +0100

    Clock divider bugfix and testbench

---
+++ reaktionszeitmesser.srcs/sim_1/new/clock_divider_tb.vhd
@@ -0,0 +1,46 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clock_divider_tb is
+end clock_divider_tb;
+
+architecture bench of clock_divider_tb is
+    constant clock_period   : time      := 10 ns;
+    signal clk              : std_logic := '0';
+    signal reset            : std_logic := '1';
+    signal clk_out          : std_logic := '0';
+    
+    component clock_divider
+        generic(f_in, f_out: integer);
+        port(
+            reset_in : in  std_logic;
+            clk_in   : in  std_logic;
+            clk_out  : out std_logic
+        );
+    end component clock_divider;
+begin
+    clk_divider: clock_divider generic map(f_in => 100e6, f_out => 10e6) port map(
+        reset_in => reset,
+        clk_in   => clk,
+        clk_out  => clk_out
+    );
+
+    
+    reset_process: process
+    begin
+        wait for 1 ns;
+        reset <= '0';
+        wait for 1 ns;
+        reset <= '1';
+        wait;
+    end process reset_process;
+
+    clock: process
+    begin
+        while true loop
+            clk <= '0', '1' after clock_period / 2;
+            wait for clock_period;
+        end loop;
+        wait;
+    end process clock;
+end bench;
--- reaktionszeitmesser.srcs/sources_1/new/clock_divider.vhd
+++ reaktionszeitmesser.srcs/sources_1/new/clock_divider.vhd
@@ -23,19 +23,20 @@ architecture Behavioral of clock_divider is
         end if;
     end num_bits;
     
-    constant max_counter: natural := f_in / f_out / 2;
-    constant counter_bits: natural := num_bits(max_counter);
+    constant max_counter  : natural := f_in / f_out / 2;
+    constant counter_bits : natural := num_bits(max_counter);
     
     signal cnt    : unsigned(counter_bits - 1 downto 0) := (others => '0');
-    signal toggle : std_logic;
+    signal toggle : std_logic := '0';
     
 begin
     cnt_update: process(reset_in, clk_in)
     begin
         if reset_in = '0' then
             cnt <= (others => '0');
+            toggle <= '0';
         elsif rising_edge(clk_in) then
-            if cnt = max_counter then
+            if cnt = max_counter - 1 then
                 cnt <= to_unsigned(0, counter_bits);
                 toggle <= not toggle;
             else
